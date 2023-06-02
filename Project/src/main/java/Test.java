c604475d4708bcb770ac99e2e3787e53e9d4d55c
Levi <gyorgydeak.levente@gmail.com>
2023-06-02 20:33:54 +0200

    update jdk to 19

--- Project/src/main/java/Test.java
+++ Project/src/main/java/Test.java
@@ -71,22 +71,24 @@ public class Test {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         try (Stream<Path> tests = Files.list(Paths.get("tests")).filter(Files::isDirectory)) {
-            var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-            tests.forEach(path -> executor.submit(() -> run(path)));
-            executor.shutdown();
+            try(var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())){
+                tests.forEach(path -> executor.submit(() -> run(path)));
+                executor.shutdown();
 
-            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                throw new RuntimeException("Tests timed out");
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    throw new RuntimeException("Tests timed out");
+                }
+
+                System.out.println();
+
+                if (failed.isEmpty()) {
+                    System.out.println("All successful!");
+                } else {
+                    System.out.println("Failed:");
+                    failed.forEach(System.out::println);
+                }
             }
 
-            System.out.println();
-
-            if (failed.isEmpty()) {
-                System.out.println("All successful!");
-            } else {
-                System.out.println("Failed:");
-                failed.forEach(System.out::println);
-            }
         }
     }
 }
